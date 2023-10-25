85e8c1aed4f8bcb1ba0bb6df601ec432eff65d10
art9440 <art9440@gmail.com>
2023-10-25 23:35:45 +0700

    master

--- lab1-0.c
+++ lab1-0.c
@@ -1,6 +1,25 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <malloc.h>
+
+#define PATTERN_SIZE 17
 
 
 int main(){
+    int patlen, textlen = 0;
+    char *text, pattern[PATTERN_SIZE], symbol;
+    gets(pattern);
+    patlen = strlen(pattern);
+    text = (char*)malloc(sizeof(char));
+    symbol = getc(stdin);
+
+    while (symbol != EOF){
+        textlen++;
+        text = (char*)realloc(text, textlen * sizeof(char));
+        *(text + textlen - 1) = (char)symbol;
+        symbol = getc(stdin);
+    }
+
     return 0;
 }
